3f70551a95406cce56dfaf788b076af7ed343312
Evan Gary <evanmgary@gmail.com>
2024-05-29 14:23:33 -0500

    remove test api

--- src/app/page.js
+++ src/app/page.js
@@ -4,9 +4,9 @@ import "./page.css"
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Link className="main-page-link" href="/grapher">Grapher</Link>
+      <Link className="main-page-link" href="/grapher">Buoy Grapher</Link>
       <Link className="main-page-link" href="/weatherapp">Weather Data</Link>
-      <Link className="main-page-link" href="/acis">ACIS</Link>
+      <Link className="main-page-link" href="/acis">ACIS Grapher</Link>
     </main>
   );
 }
