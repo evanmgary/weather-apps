351842b9de49df496c274f256dbeac5c0ef83bed
Evan Gary <evanmgary@gmail.com>
2024-10-07 13:31:31 -0500

    revalidate 0

--- src/app/api/[stationid]/route.js
+++ src/app/api/[stationid]/route.js
@@ -4,7 +4,7 @@ import parseData from '@/app/ParseData'
 
 export async function GET(request, {params}){
     const stationId = params.stationid
-    const res = await fetch("https://www.ndbc.noaa.gov/data/realtime2/" + stationId.toUpperCase() + ".txt", {next: {revalidate: 86400}})
+    const res = await fetch("https://www.ndbc.noaa.gov/data/realtime2/" + stationId.toUpperCase() + ".txt", {next: {revalidate: 0}})
 
     if (!res.ok){
         return NextResponse.json({error: "Could not obtain data"})
